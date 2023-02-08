2a36bca139e06b2ff1e90057984830d8ab81ec54
Subin Jo <sbjo1202@naver.com>
2023-02-08 22:26:43 +0900

    O

--- boj/2023/february/11000_강의실 배정.py
+++ boj/2023/february/11000_강의실 배정.py
@@ -13,8 +13,6 @@ room = []
 heapq.heappush(room, q[0][1])
 
 for i in range(1, n):
-    print('room: ', room)
-    print(q)
     if q[i][0] < room[0]:
         heapq.heappush(room, q[i][1])
     else:
