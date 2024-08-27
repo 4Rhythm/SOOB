1e3f035b3a85712266a6409e7c89de05d0e653f7
Subin Jo <sbjo1202@naver.com>
2024-08-27 11:00:26 +0900

    X

---
+++ boj/2024/august/N과 M (4).py
@@ -0,0 +1,29 @@
+# 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하기
+
+# 조건 1: 1부터 N까지 자연수 중에서 M개를 고른 수열
+# 조건 2: 같은 수를 여러 번 골라도 됨
+# 조건 3: 고른 수열은 비내림차순이어야 함 (길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 함)
+
+
+# 입력
+# 첫째 줄에 자연수 N과 M이 주어짐 (1 ≤ M ≤ N ≤ 8)
+
+# 출력
+# 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력
+# 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 함
+# 수열은 사전 순으로 증가하는 순서로 출력해야 함
+
+N, M = map(int, input().split())
+answer = []
+
+def choose(x):
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
+        return
+
+    for i in range(x, N + 1):
+        answer.append(i)
+        choose(i)
+        answer.pop()
+
+choose(1)
