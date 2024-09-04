e05b35628bec4b48d7c1ee2d11d8dd9072c9614d
Subin Jo <sbjo1202@naver.com>
2024-09-04 14:39:12 +0900

    O

---
+++ boj/2024/september/부분수열의 합.py
@@ -0,0 +1,38 @@
+# N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하는 프로그램
+
+# 입력
+# 첫째 줄에 정수의 개수를 나타내는 N과 정수 S (1 ≤ N ≤ 20, |S| ≤ 1,000,000)
+# 둘째 줄에 N개의 정수가 빈 칸을 사이에 두고 주어짐 (주어지는 정수의 절댓값은 100,000을 넘지 않음)
+
+# 출력
+# 첫째 줄에 합이 S가 되는 부분수열의 개수를 출력
+
+# 5 0
+# -7 -3 -2 5 8
+
+N, S = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+# visited = [False for _ in range(N)]
+global answer
+answer = 0
+
+def back(idx, now_sum, cnt):
+    global answer
+
+    if idx == N:
+        return
+
+    back(idx + 1, now_sum, cnt)
+
+    now_sum += numbers[idx]
+    cnt += 1
+
+    if now_sum == S:
+        answer += 1
+
+    back(idx + 1, now_sum, cnt)
+
+back(0, 0, 0)
+
+print(answer)
